70f43214e9026d69db53beecf9e6481c04569383
Anna Lebiedziewicz <nalin.kane@gmail.com>
2019-12-07 21:38:59 +0000

    added methods

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ class Teacher {
     }
 
     if (!teacherName) {
-      throw new Error("Please provide a teacher name");
+      throw new Error("Please provide a teacher name`");
     }
 
     this.subject = subject;
@@ -29,25 +29,42 @@ class Student {
 
 class Lesson {
   constructor(teacherName, studentList) {
-    // error handling
-    // ...
+    if (!teacherName) {
+      throw new Error("Please provide a teacher name`");
+    }
+
+    if (!studentList) {
+      throw new Error("Please provide a list of expected students");
+    }
 
     this.teacherName = teacherName;
     this.studentList = studentList;
+
+    console.log(this.studentList);
+    this.onTime = [];
+    this.late = [];
+    this.absent = [];
   }
 
-  markOnTime(studentList) {}
+  markOnTime(studentsOnTime) {
+    this.onTime = studentsOnTime;
+    console.log(this.onTime);
+  }
 
-  //   markLate(studentList) {}
+  markLate(studentsLate) {
+    this.late = studentsLate;
+    console.log(this.late);
+  }
+
+  // if no late or not on time mark as absent
   //   outputSummary(studentList) {
   //   }
 }
-
-const studentList = [];
-const addStudent = studentList.push(studentList);
 
 const teacher1 = new Teacher("Maths", "Paul");
 
 const student1 = new Student("Jen");
 const student2 = new Student("Eduardo");
-const mathsLesson = new Lesson(teacher1);
+const mathsLesson = new Lesson(teacher1, [student1, student2]);
+mathsLesson.markOnTime(student1);
+mathsLesson.markLate(student2);
